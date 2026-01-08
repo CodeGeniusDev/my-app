0adf9282705eb8c0e07477a97b2a560a5bd35159
Abdullah Abbad <codegenius.inc@gmail.com>
2026-01-09 01:28:30 +0500

    fix issue

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -76,6 +76,7 @@ const Card = ({
       onTouchStart={() => setHovered(true)}
       onTouchEnd={() => setHovered(false)}
       onClick={() => setHovered(!hovered)}
+      style={{ cursor: "pointer" }}
       className="border border-black/20 group/canvas-card flex text-center items-center justify-center dark:border-white/20 bg-purple-300/2 max-w-sm w-full mx-auto p-4 relative h-140 touch-manipulation"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
@@ -139,24 +140,6 @@ const AceternityIcon = ({ order }: { order: string }) => {
           />
         </svg>
       </MagicButton>
-
-      {/* <svg
-        width="66"
-        height="65"
-        viewBox="0 0 66 65"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-      >
-        <path
-          d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-          stroke="currentColor"
-          strokeWidth="15"
-          strokeMiterlimit="3.86874"
-          strokeLinecap="round"
-          style={{ mixBlendMode: "darken" }}
-        />
-      </svg> */}
     </div>
   );
 };
